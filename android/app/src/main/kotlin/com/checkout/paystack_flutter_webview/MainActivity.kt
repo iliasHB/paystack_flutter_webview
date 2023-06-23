30bfab49e67e6e31fb905571c63bdfa6187d351f
Habeeb Soliu <69279792+iliasHB@users.noreply.github.com>
2023-06-23 16:48:55 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/checkout/paystack_flutter_webview/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.checkout.paystack_flutter_webview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Paystack Webview',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const PaystackWebView(url: 'https://checkout.paystack.com/7zu1ot06d0qn9h6', reference: ''),
+    );
+  }
+}
+
+
+class PaystackWebView extends StatefulWidget {
+  final String url, reference;
+  const PaystackWebView(
+      {super.key, required this.url, required this.reference,});
+
+  @override
+  State<PaystackWebView> createState() => _PaystackWebViewState();
+}
+
+class _PaystackWebViewState extends State<PaystackWebView> {
+  late final WebViewController controller;
+  var loadingPercentage = 0;
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageStarted: (url) {
+          setState(() {
+            loadingPercentage = 0;
+          });
+        },
+        onProgress: (progress) {
+          setState(() {
+            loadingPercentage = progress;
+          });
+        },
+        onPageFinished: (url) {
+          setState(() {
+            loadingPercentage = 100;
+          });
+        },
+        onNavigationRequest: (navigation) {
+          final host = Uri.parse(navigation.url).host;
+          if (host == ('https://standard.paystack.co/close')) {
+            print("-----closing checkout----------");
+            Navigator.of(context).pop(); //close webview
+          }
+          if (host == "https://hello.pstk.xyz/callback") {
+            print("-----------------verify your payment here--------------------");
+            //verifyTransaction(reference);
+            Navigator.of(context).pop(); //close webview
+
+          }
+          return NavigationDecision.navigate;
+          //return NavigationDecision.navigate;
+        },
+      ))
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(
+        Uri.parse(widget.url),
+      )
+      ..setUserAgent('Paystack;Webview');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Colors.green,
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        elevation: 0,
+        title: const Text('Paystack WebView'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Stack(
+          children: [
+            WebViewWidget(
+              controller: controller,
+            ),
+            if (loadingPercentage < 100)
+              LinearProgressIndicator(
+                value: loadingPercentage / 100.0,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
